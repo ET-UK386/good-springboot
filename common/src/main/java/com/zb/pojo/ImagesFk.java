08472a3833173a90ee6260c9a4ff630e4db1b098
hu <1541452197hu@gmail.com>
2022-08-21 15:09:03 +0800

    新增上传模块

---
+++ common/src/main/java/com/zb/pojo/ImagesFk.java
@@ -0,0 +1,21 @@
+package com.zb.pojo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class ImagesFk {
+
+  private Long id;
+  private Long skuId;
+  private Long spuId;
+  private Long imagesPathId;
+  private Integer isLook;
+
+}
